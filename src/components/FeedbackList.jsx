9fa9f2cd88b555d82f2e6568559dd49efc3f5a12
Leonardo Ferrari <leoferrarifranca@hotmail.com>
2022-03-24 12:59:59 -0300

    Fix a bug

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,8 +2,9 @@ import { React, useContext } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
 import FeedbackItem from './FeedbackItem'
-import Spinner from './shared/Spinner'
+
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 function FeedbackList() {
   const { feedback, isLoading } = useContext(FeedbackContext)
@@ -16,20 +17,20 @@ function FeedbackList() {
     <Spinner />
   )
   : (
-  <div className="feedback-list">
-    <AnimatePresence>
-      {feedback.map((item) => (
-        <motion.div
-          key={item.id}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <FeedbackItem key={item.id} item={item} />
-        </motion.div>
-      ))}
-    </AnimatePresence>
-  </div>
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
   )
 
   // return (
